2c21e30ebb3288dcb611dd3d3cd170c1b70458f7
pablone <pablonatek002@gmail.com>
2023-03-12 21:36:31 +0100

    loginSignup y index creados y archivos restructura

--- app.js
+++ app.js
@@ -3,13 +3,21 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/public/')));
+app.use(express.static(path.join(__dirname, '/src/views/pages')));
 
-app.set('views', './src/views');
+app.set('views', './src/views/pages');
 app.set('view engine', 'ejs');
 
 app.get('/',(req, res)=>{
     res.render('index');
+});
+
+app.get('/login',(req, res)=>{
+    res.render('login');
+});
+
+app.get('/signup',(req, res)=>{
+    res.render('signup');
 });
 
 app.listen(3000, ()=>{
