91299abbe31c82db755dd21820c74be012f26602
pablone <pablonatek002@gmail.com>
2023-03-12 12:13:15 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/public/')));
+
+app.get('/',(req, res)=>{
+    res.send('hola caracola');
+});
+
+app.listen(3000, ()=>{
+    console.log('adios con dios');
+});
