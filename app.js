e5462533a006a0452325ff8a3e28dc3d6c7881a2
pablone <pablonatek002@gmail.com>
2023-03-12 20:21:14 +0100

    npm install ejs y restructura

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '/public/')));
 
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
 app.get('/',(req, res)=>{
-    res.send('hola caracola');
+    res.render('index');
 });
 
 app.listen(3000, ()=>{
