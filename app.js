f39f408c5f3f1ea386923fc83e2a50fc47f0dc80
pablone <pablonatek002@gmail.com>
2023-03-13 11:30:38 +0100

    creada configuraciónde passport y local

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const path = require('path');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/src/views/pages')));
+app.use(cookieParser());
+app.use(session({secret: 'mapster'}));
+
+require('./src/config/passport.js')(app);
 
 app.set('views', './src/views/pages');
 app.set('view engine', 'ejs');
@@ -18,6 +25,17 @@ app.get('/login',(req, res)=>{
 
 app.get('/signup',(req, res)=>{
     res.render('signup');
+});
+
+app.post('/signup/',(req, res)=>{
+    //user creation
+    req.login(req.body, ()=>{
+        res.redirect('/profile');
+    })
+});
+
+app.get('/signup/profile',(req, res)=>{
+    res.render(req.user);
 });
 
 app.listen(3000, ()=>{
