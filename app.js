b7a078b52afa5ff5b5552233308c01f16b5d74eb
pablone <pablonatek002@gmail.com>
2023-04-29 16:58:38 +0200

    añdaido boards  y board parte de login configurado

--- app.js
+++ app.js
@@ -3,10 +3,24 @@ const path = require('path');
 const passport = require('passport');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const mysql = require('mysql');
+
+let db = mysql.createConnection({
+    host: "localhost",
+    user: "node",
+    password: "node",
+    database: "mapster"
+  });
+
+  db.connect(function(err) {
+    if (err) throw err;
+    console.log("Db connected!");
+  });
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/src/views/pages')));
+app.use(express.json());
 app.use(cookieParser());
 app.use(session({secret: 'mapster'}));
 
@@ -23,21 +37,41 @@ app.get('/login',(req, res)=>{
     res.render('login');
 });
 
+app.get('/boards',(req, res)=>{
+    const boards = db.query(
+        "SELECT * FROM board", function (err, results, fields) {
+        if (err) throw err;
+        console.log(results);
+        res.render('boards', {boards: results});
+    });
+});
+
+app.get('/boards/:id', (req, res) => {
+    const id = req.params.id;
+    db.query("SELECT * FROM board WHERE id = " + id, function (err, results, fields) {
+      if (err) throw err;
+      console.log(results);
+      res.render('board', { board: results });
+    });
+  });
+  
+
 app.get('/signup',(req, res)=>{
+    console.log('signup');
     res.render('signup');
 });
 
-app.post('/signup/',(req, res)=>{
-    console.log('ricardo');
-    req.login(req.body, ()=>{
-        res.redirect('/profile');
-    })
+app.post('/signup',(req, res)=>{
+    console.log('postSignUp');
+    console.log(req.user);
+    res.json(req.body);
 });
 
 app.get('/signup/profile',(req, res)=>{
-    res.render(req.user);
+    console.log('/signup/profile');
+    console.log(req.user);
 });
 
 app.listen(3000, ()=>{
-    console.log('adios con dios');
+    console.log('Mapster Start');
 });
--- src/config/local.strategy.js
+++ src/config/local.strategy.js
@@ -3,13 +3,13 @@ const { Strategy } = require('passport-local');
 
 module.exports = function localStrategy() {
     passport.use(new Strategy({
-        usernameFiel: 'username',
+        usernameField: 'username',
         passwordField: 'password'
     },
     (username, password, done) => {
-        const user = { username, password, name: 'Ricardo' };
+        const user = {username: 'username', password: 'pass', name: 'Ricardo' };
         console.log('User log');
         done(null, user);
     }
-    ));
+    ));    
 }
